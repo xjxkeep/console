a6594408b3fd7a23f3b59c51b1cc8ce02f63595d
xjx@201 <xiongjinxin@echo.tech>
2025-02-11 22:27:12 +0800

    feat: xbox手柄显示

--- controller.py
+++ controller.py
@@ -1,8 +1,9 @@
-from qfluentwidgets import ProgressBar,SpinBox
+from qfluentwidgets import ProgressBar,SpinBox,PillPushButton
 from PyQt5.QtCore import pyqtSignal
 from PyQt5.QtWidgets import *
 import sys
 from PyQt5.QtCore import QTimer
+from pkg.joystick import JoyStick
 
 class Channel(QWidget):
     channelSignal=pyqtSignal(int)
@@ -15,21 +16,32 @@ class Channel(QWidget):
         self.fineTune.setMaximum(100)
         self.fineTune.setValue(0)
         self.channelValue=0
+        self.reverse=PillPushButton("Reverse",self)
+        layout.addWidget(self.reverse)
         layout.addWidget(self.label)
         layout.addWidget(self.progressBar)
         layout.addWidget(self.fineTune)
+        self.reverseFlag=False
+        self.reverse.clicked.connect(self.setReverse)
         self.fineTune.valueChanged.connect(self.onFineTuneChanged)
         self.progressBar.valueChanged.connect(self.channelSignal.emit)
         self.setLayout(layout)
+
         
     def __init__(self) -> None:
         super().__init__()
         self.setupUi()
-        
+    
+    
+    def setReverse(self,reverse:bool):
+        self.reverseFlag=reverse
+
     def setLabel(self,label:str):
         self.label.setText(label)
     
     def setValue(self,x:int):
+        if self.reverseFlag:
+            x=100-x
         self.channelValue=x
         self.progressBar.setValue(x+self.fineTune.value())
     
@@ -40,14 +52,13 @@ class Channel(QWidget):
         self.progressBar.setValue(self.channelValue+x)
 
 class Controller(QWidget):
-    channelSignal=pyqtSignal(list)
+    controlMessage=pyqtSignal(list)
     def setupUi(self):
         self.setWindowTitle("Controller")
         self.resize(100, 100)
         layout=QVBoxLayout()
         self.channels=[Channel() for _ in range(self.channelCount)]
         for idx,channel in enumerate(self.channels):
-            channel.setValue(10*idx+10)
             channel.setLabel(f"Channel{idx+1}")
             channel.channelSignal.connect(self.__updateChannelValues)
             layout.addWidget(channel)
@@ -60,9 +71,12 @@ class Controller(QWidget):
         self.timer=QTimer()
         self.timer.timeout.connect(self.__updateChannelValues)
         self.timer.start(1000)
+        self.joystick=JoyStick()
+        self.joystick.select_device(0)
+        self.joystick.signal.connect(self.setChannelValue)
     
     def __updateChannelValues(self):
-        self.channelSignal.emit(self.getChannelValues())
+        self.controlMessage.emit(self.getChannelValues())
     
     def getChannelValues(self):
         return [channel.getValue() for channel in self.channels]
@@ -71,6 +85,11 @@ class Controller(QWidget):
         if idx<1 or idx>self.channelCount:
             return
         self.channels[idx-1].setValue(x)
+    
+    def closeEvent(self, a0) -> None:
+        self.joystick.running=False
+        self.joystick.thread.join()
+        super().closeEvent(a0)
         
 
 if __name__ == "__main__":
--- pkg/joystick.py
+++ pkg/joystick.py
@@ -1,8 +1,82 @@
 from PyQt5.QtCore import QObject,pyqtSignal
+import pygame
+import threading
+import time
 
-
-class JoyStick(QObject):
-    joystickSignal=pyqtSignal(int,int) # (channel,value)
+class ControllerBase(QObject):
+    signal=pyqtSignal(int,int) # (channel,value)
     def __init__(self) -> None:
         super().__init__()
         
+
+class JoyStick(ControllerBase):
+    def __init__(self) -> None:
+        super().__init__()
+        # 初始化 pygame
+        
+        self.running=True
+        self.joystick = None
+        self.device_id= None
+        
+        self.thread=threading.Thread(target=self.update,daemon=True)
+        self.thread.start()
+        
+    def get_device_list(self):
+        """获取所有已连接的手柄设备列表"""
+        devices = []
+        for i in range(pygame.joystick.get_count()):
+            joy = pygame.joystick.Joystick(i)
+            devices.append({
+                'id': i,
+                'name': joy.get_name()
+            })
+        return devices
+        
+    def select_device(self, device_id: int):
+        if self.device_id==device_id:
+            return
+        self.device_id=device_id
+        try:
+            if self.joystick:
+                self.joystick.quit()
+            
+            if self.device_id >= pygame.joystick.get_count():
+                return False
+                
+            self.joystick = pygame.joystick.Joystick(self.device_id)
+            self.joystick.init()
+            return True
+        except pygame.error:
+            return False
+        
+
+    def update(self):
+        pygame.init()
+        pygame.joystick.init()
+        while self.running:
+            if not self.joystick or self.device_id is None:
+                time.sleep(0.1)
+                continue
+            time.sleep(0.02)
+            for event in pygame.event.get():
+                if event.type == pygame.JOYAXISMOTION:
+                    # 获取摇杆值（范围从-1到1）并转换为0到100
+                    axis_id = event.axis + 1  # 通道号从1开始
+                    current_value = int(event.value * 50) + 50
+                    self.signal.emit(axis_id, current_value)
+                
+                elif event.type == pygame.JOYBUTTONDOWN:
+                    # 按钮按下时发送值100
+                    channel = event.button + self.joystick.get_numaxes() + 1
+                    self.signal.emit(channel, 100)
+                    
+                elif event.type == pygame.JOYBUTTONUP:
+                    # 按钮释放时发送值0
+                    channel = event.button + self.joystick.get_numaxes() + 1
+                    self.signal.emit(channel, 0)
+    def close(self):
+        self.running=False
+        self.thread.join()
+        pygame.quit()
+
+
