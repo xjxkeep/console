1d99871761c17e77b67a0b752115b9de6b995d1e
xjx@201 <xiongjinxin@echo.tech>
2025-02-27 23:15:34 +0800

    feat: file proto

--- protocol/highway_pb2.pyi
+++ protocol/highway_pb2.pyi
@@ -2,7 +2,8 @@ from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from collections.abc import Iterable as _Iterable, Mapping as _Mapping
+from typing import ClassVar as _ClassVar, Optional as _Optional, Union as _Union
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -59,3 +60,11 @@ class Video(_message.Message):
     raw: bytes
     timestamp: int
     def __init__(self, raw: _Optional[bytes] = ..., timestamp: _Optional[int] = ...) -> None: ...
+
+class File(_message.Message):
+    __slots__ = ("name", "size")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    SIZE_FIELD_NUMBER: _ClassVar[int]
+    name: str
+    size: int
+    def __init__(self, name: _Optional[str] = ..., size: _Optional[int] = ...) -> None: ...
