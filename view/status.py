0c262c0a9359bf4493b290e8a1f545a16d88a59c
xiong jinxin <xiongjinxin@echo.tech>
2025-10-30 18:20:32 +0800

    feat: 优化状态栏控件

--- view/status.py
+++ view/status.py
@@ -1,18 +1,15 @@
 from datetime import datetime
 import logging
 
-from PyQt5.QtCore import Qt, pyqtSignal
-from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtCore import Qt, pyqtSignal, QTimer
 from PyQt5.QtGui import QCloseEvent, QColor, QIcon, QImage, QPixmap
 from PyQt5.QtWidgets import *
 from qfluentwidgets import (
-    Action,
     FlowLayout,
     FluentIcon,
-    RoundMenu,
-    TransparentDropDownPushButton,
     TransparentPushButton,
     TransparentToolButton,
+    ComboBox
 )
 
 
@@ -41,47 +38,27 @@ class StatusBar(QWidget):
         self.download=TransparentPushButton(FluentIcon.DOWN.icon(),"99 kb/s")
         self.fps=TransparentPushButton(FluentIcon.VIDEO.icon(),"30 fps")
         self.date=TransparentPushButton(FluentIcon.DATE_TIME.icon(),"2025/02/09 21:44:00")
-        self.channel=TransparentDropDownPushButton(FluentIcon.IOT.icon(),"线路: 上海")
-        self.resolution=TransparentDropDownPushButton(FluentIcon.VIDEO.icon(),"清晰度: 高清")
-        self.video_format=TransparentDropDownPushButton(FluentIcon.VIDEO.icon(),"视频格式: H.264")
-        self.bABR=TransparentPushButton(FluentIcon.VIDEO.icon(),"码率自适应: 关闭")
+        self.channel=ComboBox()
+        self.channel.addItems(["线路: 上海","线路: 北京"])
+        self.channel.setCurrentIndex(0)
+        self.channel.currentTextChanged.connect(self.__handle_channel_menu_triggered)
+        
+        self.resolution=ComboBox()
+        self.resolution.addItems(["清晰度: 高清","清晰度: 标清","清晰度: 流畅"])
+        self.resolution.setCurrentIndex(0)
+        self.resolution.currentTextChanged.connect(self.__handle_resolution_menu_triggered)
+        
+        self.video_format=ComboBox()
+        self.video_format.addItems(["视频格式: H.264","视频格式: H.265"])
+        self.video_format.setCurrentIndex(0)
+        self.video_format.currentTextChanged.connect(self.__handle_video_format_menu_triggered)
+        
+
+        self.bABR=ComboBox()
+        self.bABR.addItems(["码率自适应: 开启","码率自适应: 关闭"])
+        self.bABR.setCurrentIndex(0)
+        self.bABR.currentTextChanged.connect(self.__handle_bABR_menu_triggered)
         self.battery=TransparentPushButton(QIcon("assets/svg/battery-full.svg"),"100%")
-
-        switch_menu=RoundMenu(parent=self)
-        switch_menu.addActions([
-            Action('开启'),
-            Action('关闭'),
-        ])
-        self.bABR.setMenu(switch_menu)
-        switch_menu.triggered.connect(self.__handle_bABR_menu_triggered)
-
-
-        channel_menu = RoundMenu(parent=self)
-        channel_menu.addActions([
-            Action('上海'),
-            Action('北京'),
-        ])
-        self.channel.setMenu(channel_menu)
-        channel_menu.triggered.connect(self.__handle_channel_menu_triggered)
-
-
-        video_format_menu=RoundMenu(parent=self)
-        video_format_menu.addActions([
-            Action('H.264'),
-            Action('H.265'),
-        ])
-        self.video_format.setMenu(video_format_menu)
-        video_format_menu.triggered.connect(self.__handle_video_format_menu_triggered)
-
-
-        resolution_menu=RoundMenu(parent=self)
-        resolution_menu.addActions([
-            Action('高清'),
-            Action('标清'),
-            Action('流畅'),
-        ])
-        self.resolution.setMenu(resolution_menu)
-        resolution_menu.triggered.connect(self.__handle_resolution_menu_triggered)
 
         self.timer=QTimer(self)
         self.timer.setInterval(1000)
@@ -110,28 +87,24 @@ class StatusBar(QWidget):
 
     def __handel_setting_changed(self):
          self.param_changed.emit({
-            "resolution":self.resolution.text().split(":")[1].strip(),
-            "video_format":self.video_format.text().split(":")[1].strip(),
-            "channel":self.channel.text().split(":")[1].strip(),
-            "bABR":self.bABR.text().split(":")[1].strip(),
+            "resolution":self.resolution.currentText().split(":")[1].strip(),
+            "video_format":self.video_format.currentText().split(":")[1].strip(),
+            "channel":self.channel.currentText().split(":")[1].strip(),
+            "bABR":self.bABR.currentText().split(":")[1].strip(),
         })
     
-    def __handle_channel_menu_triggered(self,action:Action):
-        logging.info(action.text())
-        self.channel.setText("线路: "+action.text())
+    def __handle_channel_menu_triggered(self,text:str):
+        logging.info(text)
         self.__handel_setting_changed()
 
-    def __handle_video_format_menu_triggered(self,action:Action):
-        logging.info(action.text())
-        self.video_format.setText("视频格式: "+action.text())
+    def __handle_video_format_menu_triggered(self,text:str):
+        logging.info(text)
         self.__handel_setting_changed()
-    def __handle_resolution_menu_triggered(self,action:Action):
-        logging.info(action.text())
-        self.resolution.setText("清晰度: "+action.text())
+    def __handle_resolution_menu_triggered(self,text:str):
+        logging.info(text)
         self.__handel_setting_changed()
-    def __handle_bABR_menu_triggered(self,action:Action):
-        logging.info(action.text())
-        self.bABR.setText("码率自适应: "+action.text())
+    def __handle_bABR_menu_triggered(self,text:str):
+        logging.info(text)
         self.__handel_setting_changed()
     def __init__(self):
         super().__init__()
